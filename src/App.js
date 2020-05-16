73bd6eac9e3d6ffcacc12e517422ecae3521f13e
Samyak <samdalal1021@gmail.com>
2020-05-16 17:44:49 +0530

    New Libraries

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function App(props) {
+  const { text } = props;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{text}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
