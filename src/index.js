8316a71a7c3c3eda70d1e49fce38f094fc9e0a9f
Samyak <samdalal1021@gmail.com>
2020-05-19 23:21:03 +0530

    Kuch nahi hora

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Input } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App text="Hello world!" />
-  </React.StrictMode>,
-  document.getElementById("root")
+const { Search } = Input;
+
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: '#1890ff',
+    }}
+  />
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <>
+    <Search
+      placeholder="input search text"
+      onSearch={value => console.log(value)}
+      style={{ width: 200 }}
+    />
+    <br />
+    <br />
+    <Search placeholder="input search text" onSearch={value => console.log(value)} enterButton />
+    <br />
+    <br />
+    <Search
+      placeholder="input search text"
+      enterButton="Search"
+      size="large"
+      onSearch={value => console.log(value)}
+    />
+    <br />
+    <br />
+    <Search
+      placeholder="input search text"
+      enterButton="Search"
+      size="large"
+      suffix={suffix}
+      onSearch={value => console.log(value)}
+    />
+  </>,
+  document.getElementById('container'),
+);
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App text="Hello world!" />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
