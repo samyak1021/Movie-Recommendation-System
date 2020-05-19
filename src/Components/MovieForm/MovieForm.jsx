50ba6f18fc2446d03d8dfb2a60bc5d7a5a6e35b1
Samyak <samdalal1021@gmail.com>
2020-05-20 00:17:58 +0530

    Edit Form with Ant

--- src/Components/MovieForm/MovieForm.jsx
+++ src/Components/MovieForm/MovieForm.jsx
@@ -1,33 +1,27 @@
 import React from "react";
 import "./MovieForm.css";
-
+import { Input } from 'antd';
+const { Search } = Input;
 class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { movieName: "" };
-  }
-
-  handleSubmit = (event) => {
-    const { onSubmit } = this.props;
-    const { movieName } = this.state;
-
-    event.preventDefault();
-    onSubmit(movieName);
-  };
-
   render() {
+    const { onSubmit } = this.props;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <span className="form_text">&#x3C;Form /&#x3E;</span>
-        <input
-          type="text"
-          value={this.state.movieName}
-          onChange={(event) => this.setState({ movieName: event.target.value })}
-          placeholder="Enter Movie Name"
-          required
-        />
-        <button>Go!</button>
-      </form>
+      <Search
+      placeholder="input search text"
+      onSearch={onSubmit}
+      style={{ width: 200 }}
+    />
+      // <form onSubmit={this.handleSubmit}>
+      //   <span className="form_text">&#x3C;Form /&#x3E;</span>
+      //   <input
+      //     type="text"
+      //     value={this.state.movieName}
+      //     onChange={(event) => this.setState({ movieName: event.target.value })}
+      //     placeholder="Enter Movie Name"
+      //     required
+      //   />
+      //   <button>Go!</button>
+      // </form>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,65 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import 'antd/dist/antd.css';
-import './index.css';
-import { Input } from 'antd';
-import { AudioOutlined } from '@ant-design/icons';
-
-const { Search } = Input;
-
-const suffix = (
-  <AudioOutlined
-    style={{
-      fontSize: 16,
-      color: '#1890ff',
-    }}
-  />
-);
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <>
-    <Search
-      placeholder="input search text"
-      onSearch={value => console.log(value)}
-      style={{ width: 200 }}
-    />
-    <br />
-    <br />
-    <Search placeholder="input search text" onSearch={value => console.log(value)} enterButton />
-    <br />
-    <br />
-    <Search
-      placeholder="input search text"
-      enterButton="Search"
-      size="large"
-      onSearch={value => console.log(value)}
-    />
-    <br />
-    <br />
-    <Search
-      placeholder="input search text"
-      enterButton="Search"
-      size="large"
-      suffix={suffix}
-      onSearch={value => console.log(value)}
-    />
-  </>,
-  document.getElementById('container'),
+  <React.StrictMode>
+    <App text="Hello world!" />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App text="Hello world!" />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
