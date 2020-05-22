01e1763a6e083f3ff4ad8c228a861f22a297d185
Samyak <samdalal1021@gmail.com>
2020-05-22 22:27:13 +0530

    Add Autocomplete

--- src/Components/MovieForm/MovieForm.jsx
+++ src/Components/MovieForm/MovieForm.jsx
@@ -1,27 +1,73 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./MovieForm.css";
-import { Input } from 'antd';
+import { Input, AutoComplete } from 'antd';
 const { Search } = Input;
 class Form extends React.Component {
   render() {
     const { onSubmit } = this.props;
+    function getRandomInt(max, min = 0) {
+      return Math.floor(Math.random() * (max - min + 1)) + min; // eslint-disable-line no-mixed-operators
+    }
+    
+    const searchResult = query =>
+      new Array(getRandomInt(5))
+        .join('.')
+        .split('.')
+        .map((item, idx) => {
+          const category = `${query}${idx}`;
+          return {
+            value: category,
+            label: (
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <span>
+                  Found {query} on{' '}
+                  <a
+                    href={`https://api.themoviedb.org/3/movie/76341?api_key="0f8d529ca28503395a1f7dc2532ad517"{query}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {category}
+                  </a>
+                </span>
+                <span>{getRandomInt(200, 100)} results</span>
+              </div>
+            ),
+          };
+        });
+    
+    const Complete = () => {
+      const [options, setOptions] = useState([]);
+    
+      // const handleSearch = value => {
+      //   setOptions(value ? searchResult(value) : []);
+      // };
+    
+      const onSelect = value => {
+        console.log('onSelect', value);
+      };
+    
     return (
-      <Search
-      placeholder="input search text"
+      <AutoComplete
+      dropdownMatchSelectWidth={252}
+      style={{
+        width: 300,
+      }}
+      options={options}
+      onSelect={onSelect}
       onSearch={onSubmit}
-      style={{ width: 200 }}
-    />
-      // <form onSubmit={this.handleSubmit}>
-      //   <span className="form_text">&#x3C;Form /&#x3E;</span>
-      //   <input
-      //     type="text"
-      //     value={this.state.movieName}
-      //     onChange={(event) => this.setState({ movieName: event.target.value })}
-      //     placeholder="Enter Movie Name"
-      //     required
-      //   />
-      //   <button>Go!</button>
-      // </form>
+    >
+      <Input.Search size="large" placeholder="input here" enterButton />
+    </AutoComplete>
+    //   <Search
+    //   placeholder="input search text"
+    //   onSearch={onSubmit}
+    //   style={{ width: 200 }}
+    // />
     );
   }
 }
